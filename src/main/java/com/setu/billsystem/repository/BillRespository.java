0a1df9d034342de67ac0503b4ec9e74c4435958a
rupeshchaudhari <rupeshachaudhari@gmail.com>
2020-04-06 21:30:52 +0400

    Made changes to Bill Repository

--- src/main/java/com/setu/billsystem/repository/BillRespository.java
+++ src/main/java/com/setu/billsystem/repository/BillRespository.java
@@ -21,8 +21,8 @@ public class BillRespository{
 		bills.add(new Bill("12123131322", "2020-04-02T19:49:00Z", "ONE_TIME", "EXACT", new CustomerAccount("8208021440", "Rupesh"), new Aggregates(new Total("Total Outstanding", new Amount(99000.00)))));
 		bills.add(new Bill("12123131123", "2020-04-10T19:20:00Z", "MONTHLY", "EXACT", new CustomerAccount("8481779999", "Ashok"), new Aggregates(new Total("Total Outstanding", new Amount(75999.50)))));
 		bills.add(new Bill("12123131124", "2020-04-13T19:49:00Z", "YEARLY", "EXACT", new CustomerAccount("8481778080", "Puneet"), new Aggregates(new Total("Total Outstanding", new Amount(80999.50)))));
-		bills.add(new Bill("12123131125", "2020-04-13T19:49:00Z", "ONE_TIME", "EXACT", new CustomerAccount("8481778080", "Puneet"), new Aggregates(new Total("Total Outstanding", new Amount(73.23)))));
-		bills.add(new Bill("12123131126", "2020-04-13T19:49:00Z", "MONTHLY", "EXACT", new CustomerAccount("8481778080", "Puneet"), new Aggregates(new Total("Total Outstanding", new Amount(102123.23)))));
+		bills.add(new Bill("12123131125", "2020-04-14T19:49:00Z", "ONE_TIME", "EXACT", new CustomerAccount("8481778080", "Puneet"), new Aggregates(new Total("Total Outstanding", new Amount(73.23)))));
+		bills.add(new Bill("12123131126", "2020-04-15T19:49:00Z", "MONTHLY", "EXACT", new CustomerAccount("8481778080", "Puneet"), new Aggregates(new Total("Total Outstanding", new Amount(102123.23)))));
 
 	}
 
